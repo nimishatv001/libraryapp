e33163fae5f1a8efc4e13c4409410dd09ef508ec
nimisha <nimisha.tv001@gmail.com>
2022-09-28 20:58:21 +0530

    port var changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const path = require('path')
 
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 
 const authRoute = require('./routes/auth-route')
 const mongoose = require('mongoose');
